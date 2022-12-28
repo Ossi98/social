a06b0142909d22f9fe6af30c5b97cb4c17e16813
Ossama Hassari <hassariossama98@gmail.com>
2022-12-28 18:05:25 +0100

    new vr

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 
+
 from app import models
 from app.database import engine
 from .routers import post, user, auth
+
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -15,4 +17,10 @@ app.include_router(auth.router)
 
 @app.get("/")
 async def root() -> dict:
-    return {"message": "Hello World"}
+    """    
+    '''$2y$10$TwbInXyE4J/Hmi9gOIXySeWsOBfptzYQZgn.8qMxZLpyC4gFpVJLa = 123456'''
+    user = models.User(email="test@test.com",password='$2y$10$TwbInXyE4J/Hmi9gOIXySeWsOBfptzYQZgn.8qMxZLpyC4gFpVJLa')
+    db.add(user)
+    db.commit()
+    db.refresh(user)"""
+    return {"message": "Hello World Mimi"}
